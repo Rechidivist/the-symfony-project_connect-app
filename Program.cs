ec3a9535934dae6792ebb4e1469bcd8079f898bf
Denis Beloedoff <den.beloedoff2011@yandex.ru>
2020-03-14 20:00:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Topshelf;
+
+namespace SiteConnectorService
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        { 
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<Service>(s =>
+                {
+                    s.ConstructUsing(name => new Service());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Сервис обновляющий инфу с сайта");
+                x.SetDisplayName("The HardWorker SERIAL <-> SITE interface");
+                x.SetServiceName("HardWorker");
+            });                                                            
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode()); 
+            Environment.ExitCode = exitCode;
+        }
+
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SiteConnectorService
+{
+    class Service
+    {
+        private IConnection connection;
+        private IModel channel;
+
+        public Service()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port=5627};
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(consumer, "controllerQueue",true);
+            consumer.Received += Consumer_Received;
+
+        }
+
+        public void Start()
+        { 
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+        }
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Send(Encoding.UTF8.GetString(e.Body));
+        }
+
+        private void Send(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Stop()
+        {
+            channel.Close(0,"ServiceStoped");
+            connection.Close(0, "ServiceStoped");
+        }
+    }
+}
